5171cbfb6d1d5a24bc05c3f11727dc110055b37b
jipingxu <xjpdyx@hotmail.com>
2019-07-31 15:00:54 +0800

    小修改

--- cloud-pets/src/main/java/com/cloud/pets/controller/PartyInController.java
+++ cloud-pets/src/main/java/com/cloud/pets/controller/PartyInController.java
@@ -1,6 +1,7 @@
 package com.cloud.pets.controller;
 
 
+import io.swagger.annotations.Api;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -14,6 +15,7 @@ import org.springframework.web.bind.annotation.RestController;
  */
 @RestController
 @RequestMapping("/partyIn")
+@Api(tags = "聚会参与")
 public class PartyInController {
 
 }
--- cloud-pets/src/main/java/com/cloud/pets/controller/RemindTypeController.java
+++ cloud-pets/src/main/java/com/cloud/pets/controller/RemindTypeController.java
@@ -1,6 +1,7 @@
 package com.cloud.pets.controller;
 
 
+import io.swagger.annotations.Api;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -14,6 +15,7 @@ import org.springframework.web.bind.annotation.RestController;
  */
 @RestController
 @RequestMapping("/remindType")
+@Api(tags = "提醒类型")
 public class RemindTypeController {
 
 }
