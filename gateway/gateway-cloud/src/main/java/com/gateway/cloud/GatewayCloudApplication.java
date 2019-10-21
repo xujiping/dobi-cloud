4876997850bfdee3d41ff6c0bbb0443fbd35cf38
jipingxu <xjpdyx@hotmail.com>
2019-10-21 16:37:11 +0800

    创建spring cloud gateway项目，聚合api成功

---
+++ gateway/gateway-cloud/src/main/java/com/gateway/cloud/GatewayCloudApplication.java
@@ -0,0 +1,23 @@
+package com.gateway.cloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+/**
+ * @author xujiping
+ * @date 2019-10-21 16:01
+ */
+@SpringBootApplication(exclude = {HibernateJpaAutoConfiguration.class, DataSourceAutoConfiguration.class})
+@EnableCaching
+@EnableDiscoveryClient
+public class GatewayCloudApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GatewayCloudApplication.class, args);
+    }
+
+}
