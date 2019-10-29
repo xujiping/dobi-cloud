15e321d74ac299b51fed82d0ed9ec83422c3d26b
jipingxu <xjpdyx@hotmail.com>
2019-10-29 17:52:40 +0800

    网关中心获取token

--- gateway/gateway-cloud/src/main/java/com/gateway/cloud/filter/TokenFilter.java
+++ gateway/gateway-cloud/src/main/java/com/gateway/cloud/filter/TokenFilter.java
@@ -4,10 +4,13 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.cloud.gateway.filter.GatewayFilterChain;
 import org.springframework.cloud.gateway.filter.GlobalFilter;
 import org.springframework.core.Ordered;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.server.reactive.ServerHttpRequest;
 import org.springframework.stereotype.Component;
 import org.springframework.web.server.ServerWebExchange;
 import reactor.core.publisher.Mono;
+
+import java.util.List;
 
 
 /**
@@ -24,6 +27,11 @@ public class TokenFilter implements GlobalFilter, Ordered {
     @Override
     public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
         ServerHttpRequest request = exchange.getRequest();
+        HttpHeaders headers = request.getHeaders();
+        if(headers.containsKey("ucToken")){
+            List<String> tokens = headers.get("ucToken");
+            log.info("token=" + tokens.get(0));
+        }
         // todo 在网关中心处理校验
         return chain.filter(exchange);
     }
